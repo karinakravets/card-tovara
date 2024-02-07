6a97e57af058439e03a08f5ab7d5e44b15fa5f65
karina <kraveck45@gmail.com>
2024-02-07 11:45:01 +0300

    Initial commit

---
+++ app/src/main/java/kravets/k/card_tovara/MainActivity.kt
@@ -0,0 +1,11 @@
+package kravets.k.card_tovara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
